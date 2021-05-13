ceea56af2e1386f92568fdc4a3acdf131c7a4f23
Lml <1093185442@qq.com>
2021-05-13 20:30:54 +0800

    styled 样式模板组件创建

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
 import './App.css';
-import { Button, Card } from "antd"
-import styled from "@emotion/styled"
-
-const Container = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-min-height: 100vh;
-`
-
+import { Button} from "antd"
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <Card>
           <Button type="primary" >按钮</Button>
-
-        </Card>
-
-      </Container>
     </div>
   );
 }
